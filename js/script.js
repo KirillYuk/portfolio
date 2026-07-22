37e96705821b31176117778147d43dccfae39607
KirillYuk <qwertyunisky@gmail.com>
2026-07-22 03:20:29 +0300

    style: add animations + js

---
+++ js/script.js
@@ -0,0 +1,37 @@
+const links = document.querySelectorAll(".navigation a");
+
+links.forEach((link) => {
+  link.addEventListener("click", () => {
+    const id = link.getAttribute("href");
+    const section = document.querySelector(id);
+
+    if (!section) return;
+
+    const observer = new IntersectionObserver(
+      (entries, observer) => {
+        entries.forEach((entry) => {
+          if (!entry.isIntersecting) return;
+
+          const title = entry.target.querySelector("h2");
+
+          title.classList.add("highlight-title");
+
+          title.addEventListener(
+            "animationend",
+            () => {
+              title.classList.remove("highlight-title");
+            },
+            { once: true },
+          );
+
+          observer.disconnect();
+        });
+      },
+      {
+        threshold: 1,
+      },
+    );
+
+    observer.observe(section);
+  });
+});
